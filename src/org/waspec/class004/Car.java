db300b2d51a679a01eb095dd7f3041a940ab155d
Majiong Jiang <mjjiang@gmail.com>
2015-02-02 21:49:12 -0800

    Add Car.java

---
+++ src/org/waspec/class004/Car.java
@@ -0,0 +1,12 @@
+package org.waspec.class004;
+
+/**
+ * Created by Majiong on 01/30/15.
+ */
+class Car {
+    public int speed;
+    public int owner;
+    public void run(){
+        System.out.println("I am running!");
+    }
+}
